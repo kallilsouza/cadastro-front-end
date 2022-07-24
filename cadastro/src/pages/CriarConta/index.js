22f0ca5d1672bf5c6807dfe1b5abe404b0af1e1c
Kallil <kallilsouza@hotmail.com>
2022-07-23 22:50:43 -0300

    cadastro com verificação de erros

--- cadastro/src/pages/CriarConta/index.js
+++ cadastro/src/pages/CriarConta/index.js
@@ -14,9 +14,15 @@ const CriarConta = () => {
             navigate('/home');
         }
     }, [navigate]);
-    const { register, handleSubmit } = useForm();    
+    const { register, handleSubmit, formState: errors, setError } = useForm();    
 
     const onSubmit = useCallback(async (formData) => {
+        if(formData.password !== formData.password2){
+            setError("password", {
+                type: "validate",
+                message: "As senhas devem ser iguais"
+            });
+        }
         try{
             const res = await api.post("usuarios/", formData);
             if(res.status === 201){
@@ -24,10 +30,28 @@ const CriarConta = () => {
                 navigate('/login');
             }
         }
-        catch (err){
+        catch (error){
+            if(error.response.data.cpf){
+                setError("cpf", {
+                    type: "validate",
+                    message: error.response.data.cpf,
+                });
+            }
+            if(error.response.data.pis){
+                setError("pis", {
+                    type: "validate",
+                    message: error.response.data.pis,
+                });
+            }
+            if(error.response.data.email){
+                setError("email", {
+                    type: "validate",
+                    message: error.response.data.email,
+                });
+            }
             alert("Ocorreu um erro, verifique o formulário.");
         }
-      }, [navigate]);
+      }, [navigate, setError]);
 
     return (
         <div className="cadastro">
@@ -35,34 +59,40 @@ const CriarConta = () => {
             <form className="cadastro-form" onSubmit={handleSubmit(onSubmit)}>
                 <fieldset>
                     <legend>Informações de usuário</legend>
-                    <label for="nome">Nome</label>
+                    <label htmlFor="nome">Nome</label>
                     <input type="text" name="nome" placeholder="Maria da Silva" {...register("nome", { required: true })}></input><br />
-                    <label for="email">E-mail</label>
-                    <input type="text" name="email" placeholder="maria@email.com" {...register("email", { required: true })}></input><br />
-                    <label for="senha">Senha</label>
-                    <input type="password" name="password" placeholder="1a952ad77q" {...register("password")}></input>           
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" name="email" placeholder="maria@email.com" {...register("email", { required: true })}></input><br />
+                    {errors.errors?.email && (<div className="form-erro">{errors.errors?.email?.message}</div>)}
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" name="password" placeholder="1a952ad77q" {...register("password")}></input><br />
+                    <label htmlFor="confirm-password">Confirmar</label>
+                    <input type="password" name="confirm-password" placeholder="1a952ad77q" {...register("password2")}></input><br />                   
+                    {errors.errors?.password && (<div className="form-erro">{errors.errors?.password?.message}</div>)}
                 </fieldset>
                 <fieldset>
                         <legend>Documentos (apenas números):</legend>
-                        <label for="cpf">CPF</label>
+                        <label htmlFor="cpf">CPF</label>
                         <input type="text" name="cpf" placeholder="64791138023" {...register("cpf", { required: true })}></input><br />
-                        <label for="pis">PIS</label>
-                        <input type="text" name="pis" placeholder="04283359229" {...register("pis", { required: true })}></input>     
+                        {errors.errors?.cpf && (<div className="form-erro">{errors.errors?.cpf?.message}</div>)}                        
+                        <label htmlFor="pis">PIS</label>
+                        <input type="text" name="pis" placeholder="04283359229" {...register("pis", { required: true })}></input>  
+                        {errors.errors?.pis && (<div className="form-erro">{errors.errors?.pis?.message}</div>)}                           
                 </fieldset>
                 <fieldset>
                         <legend>Endereço:</legend>
-                        <label for="pais">País</label>
+                        <label htmlFor="pais">País</label>
                         <input type="text" name="pais" placeholder="Brasil" {...register("endereco.pais", { required: true })}></input><br />
-                        <label for="estado">Estado</label>
+                        <label htmlFor="estado">Estado</label>
                         <input type="text" name="estado" placeholder="São Paulo" {...register("endereco.estado", { required: true })}></input><br />
-                        <label for="municipio">Município</label>
+                        <label htmlFor="municipio">Município</label>
                         <input type="text" name="municipio" placeholder="Diadema" {...register("endereco.municipio", { required: true })}></input><br />
-                        <label for="cep">CEP</label>
-                        <input type="text" name="cep" placeholder="09910-720" {...register("endereco.cep", { required: true })}></input><br />
-                        <label for="rua">Rua</label>
+                        <label htmlFor="cep">CEP</label>
+                        <input type="text" name="cep" placeholder="09910-720" {...register("endereco.cep", { required: true, maxLength: 8 })}></input><br />  
+                        <label htmlFor="rua">Rua</label>
                         <input type="text" name="rua" placeholder="R. Manoel da Nóbrega" {...register("endereco.rua", { required: true })}></input>  <br />
-                        <label for="numero">Número</label>
-                        <input type="text" name="numero" placeholder="712" {...register("endereco.numero", { required: true })}></input>            
+                        <label htmlFor="numero">Número</label>
+                        <input type="text" name="numero" placeholder="712" {...register("endereco.numero",{ required: true, maxLength: 15 })}></input>            
                 </fieldset>
                 <button type="submit">criar conta</button>  
                 <br />
