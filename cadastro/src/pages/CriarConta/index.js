8840289d76a30ff41c9ea9a569fbdb06b609445d
Kallil <kallilsouza@hotmail.com>
2022-07-23 22:04:11 -0300

    Cadastro com fieldsets

--- cadastro/src/pages/CriarConta/index.js
+++ cadastro/src/pages/CriarConta/index.js
@@ -33,26 +33,37 @@ const CriarConta = () => {
         <div className="cadastro">
             <h1>Cadastro</h1>
             <form className="cadastro-form" onSubmit={handleSubmit(onSubmit)}>
-                <h2>Informações de usuário:</h2>
-                <div className="cadastro-form-inputs">
-                    <input type="text" name="nome" placeholder="nome" {...register("nome", { required: true })}></input>
-                    <input type="text" name="email" placeholder="e-mail" {...register("email", { required: true })}></input>
-                    <input type="password" name="password" placeholder="senha" {...register("password")}></input>             
-                </div>
-                <h2>Documentos (apenas números):</h2>
-                <div className="cadastro-form-inputs">
-                    <input type="text" name="cpf" placeholder="CPF" {...register("cpf", { required: true })}></input>
-                    <input type="text" name="pis" placeholder="PIS" {...register("pis", { required: true })}></input>     
-                </div>
-                <h2>Endereço:</h2>
-                <div className="cadastro-form-inputs">  
-                    <input type="text" name="pais" placeholder="país" {...register("endereco.pais", { required: true })}></input>
-                    <input type="text" name="estado" placeholder="estado" {...register("endereco.estado", { required: true })}></input>
-                    <input type="text" name="municipio" placeholder="município" {...register("endereco.municipio", { required: true })}></input>
-                    <input type="text" name="cep" placeholder="CEP" {...register("endereco.cep", { required: true })}></input>
-                    <input type="text" name="rua" placeholder="rua" {...register("endereco.rua", { required: true })}></input>  
-                    <input type="text" name="numero" placeholder="número" {...register("endereco.numero", { required: true })}></input>            
-                </div>
+                <fieldset>
+                    <legend>Informações de usuário</legend>
+                    <label for="nome">Nome</label>
+                    <input type="text" name="nome" placeholder="Maria da Silva" {...register("nome", { required: true })}></input><br />
+                    <label for="email">E-mail</label>
+                    <input type="text" name="email" placeholder="maria@email.com" {...register("email", { required: true })}></input><br />
+                    <label for="senha">Senha</label>
+                    <input type="password" name="password" placeholder="1a952ad77q" {...register("password")}></input>           
+                </fieldset>
+                <fieldset>
+                        <legend>Documentos (apenas números):</legend>
+                        <label for="cpf">CPF</label>
+                        <input type="text" name="cpf" placeholder="64791138023" {...register("cpf", { required: true })}></input><br />
+                        <label for="pis">PIS</label>
+                        <input type="text" name="pis" placeholder="04283359229" {...register("pis", { required: true })}></input>     
+                </fieldset>
+                <fieldset>
+                        <legend>Endereço:</legend>
+                        <label for="pais">País</label>
+                        <input type="text" name="pais" placeholder="Brasil" {...register("endereco.pais", { required: true })}></input><br />
+                        <label for="estado">Estado</label>
+                        <input type="text" name="estado" placeholder="São Paulo" {...register("endereco.estado", { required: true })}></input><br />
+                        <label for="municipio">Município</label>
+                        <input type="text" name="municipio" placeholder="Diadema" {...register("endereco.municipio", { required: true })}></input><br />
+                        <label for="cep">CEP</label>
+                        <input type="text" name="cep" placeholder="09910-720" {...register("endereco.cep", { required: true })}></input><br />
+                        <label for="rua">Rua</label>
+                        <input type="text" name="rua" placeholder="R. Manoel da Nóbrega" {...register("endereco.rua", { required: true })}></input>  <br />
+                        <label for="numero">Número</label>
+                        <input type="text" name="numero" placeholder="712" {...register("endereco.numero", { required: true })}></input>            
+                </fieldset>
                 <button type="submit">criar conta</button>  
                 <br />
                 <Link to="/login" className="login-link">Login</Link>
